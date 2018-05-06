67293b8c9082439f6f748877232424d5542a928b
yangyi <yangyirunning@163.com>
2018-05-06 18:23:22 +0800

    2018年05月06日18:23:22

--- src/ds/LinkCreate.java
+++ src/ds/LinkCreate.java
@@ -1,7 +1,7 @@
 package ds;
 
 /**
- * 线性表的创建（基于链式存储的逻辑结构）
+ * 线性表的创建（基于链式存储的逻辑结构）头插法和尾插法
  *
  * @author yangyi 2018年05月06日15:07:45
  */
@@ -34,16 +34,16 @@ public class LinkCreate {
     /**
      * 头插法插入数据元素
      *
-     * @param data   即将被插的线性表
-     * @param object 插入的一排数据
+     * @param data    即将被插的线性表
+     * @param objects 插入的一排数据
      * @return
      */
-    private LinkTable headCreate(LinkTable data, Object[] object) {
+    private LinkTable headCreate(LinkTable data, Object[] objects) {
         Node p = data.node;
 
-        for (int i = 0; i < object.length; i++) {
+        for (int i = 0; i < objects.length; i++) {
             Node node = new Node();
-            node.object = object[i];
+            node.object = objects[i];
             node.next = p.next;
             p.next = node;
         }
@@ -52,12 +52,75 @@ public class LinkCreate {
         return data;
     }
 
+    /**
+     * 尾插法插入数据元素
+     *
+     * @param data    即将被插的线性表
+     * @param objects 插入的一排数据
+     * @return
+     */
+    private LinkTable tailCreate(LinkTable data, Object[] objects) {
+        //p指向线性表的头节点
+        Node p = data.node;
+
+        for (int i = 0; i < objects.length; i++) {
+            Node node = new Node();
+            node.object = objects[i];
+            p.next = node;
+            p = node;
+        }
+
+        p.next = null;
+        //被插完后返回
+        return data;
+    }
+
+    /**
+     * 整表清空
+     *
+     * @param data 要清空的线性表
+     * @return
+     */
+    private LinkTable clearLink(LinkTable data) {
+        for (Node p = data.node; p != null; p = p.next) {
+            Node q = p.next;
+            //释放p
+            p.object = null;
+            p.next = null;
+            //这个干掉了再接着去干下一个
+            p = q;
+        }
+        return data;
+    }
+
     public static void main(String[] args) {
         LinkCreate linkCreate = new LinkCreate();
-        LinkTable linkTable = new LinkTable();
-        LinkTable linkTable1 = linkCreate.headCreate(linkTable, new String[]{"我", "上", "楼", "复", "习"});
 
-        for (Node node = linkTable1.node.next; node != null; node = node.next) {
+        System.out.println("头插法插入数据后打印出来看看：");
+        LinkTable linkTable = new LinkTable();
+        LinkTable linkTable2 = linkCreate.headCreate(linkTable, new String[]{"我", "上", "楼", "复", "习"});
+        for (Node node = linkTable2.node.next; node != null; node = node.next) {
+            System.out.println(node.object);
+        }
+
+        System.out.println();
+        System.out.println("尾插法插入数据后打印出来看看：");
+        LinkTable linkTableTail = new LinkTable();
+        LinkTable linkTableTail2 = linkCreate.tailCreate(linkTableTail, new String[]{"我", "上", "楼", "复", "习"});
+        for (Node node = linkTableTail2.node.next; node != null; node = node.next) {
+            System.out.println(node.object);
+        }
+
+        System.out.println();
+        System.out.println("清除头插法构造的线性表将其输出：");
+        linkCreate.clearLink(linkTable2);
+        for (Node node = linkTable2.node.next; node != null; node = node.next) {
+            System.out.println(node.object);
+        }
+
+        System.out.println("清除尾插法构造的线性表将其输出：");
+        linkCreate.clearLink(linkTableTail2);
+        for (Node node = linkTableTail2.node.next; node != null; node = node.next) {
             System.out.println(node.object);
         }
     }
