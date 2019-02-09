2c6b0169f0639f724ab24343094e1a271f523751
yangyi <yangyirunning@163.com>
2019-02-09 11:28:35 +0800

    合并两个有序数组

---
+++ src/ds/MergeArray.java
@@ -0,0 +1,29 @@
+package ds;
+
+/**
+ * 合并两个有序数组
+ * <p>
+ * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ * <p>
+ * 说明:
+ * <p>
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ *
+ * @author yangyi 2019年02月09日11:10:35
+ */
+public class MergeArray {
+
+    public int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = m-- + n-- - 1;
+        while (m >= 0 && n >= 0) {
+            nums1[p--] = nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+
+        while (n >= 0) {
+            nums1[p--] = nums2[n--];
+        }
+        return nums1;
+    }
+
+}
