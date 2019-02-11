e22431c9f4a2052c61c28d0bd3d0a422fa1e58b2
yangyi <yangyirunning@163.com>
2019-02-11 23:15:21 +0800

    换种方式解斐波拉切

--- src/ds/FibonacciArray.java
+++ src/ds/FibonacciArray.java
@@ -14,9 +14,28 @@ public class FibonacciArray {
         return fibonacciArray(n - 1) + fibonacciArray(n - 2);
     }
 
+    public int fibonacciArrayForeach(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int pre = 1;
+        int prepre = 0;
+        int sum = 0;
+        for (int i = 2; i <= n; i++) {
+            sum = pre + prepre;
+            prepre = pre;
+            pre = sum;
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
         FibonacciArray fibonacciArray = new FibonacciArray();
-        int n = 9;
+        int n = 10;
         System.out.println("斐波拉切数列位置为" + n + "处所对应的结果为:" + fibonacciArray.fibonacciArray(n));
+        System.out.println("斐波拉切数列位置为" + n + "处所对应的结果为:" + fibonacciArray.fibonacciArrayForeach(n));
     }
 }
