6adc694917dfa1178cbcd810c6dcfea814c765d0
yangyi <yangyi@rongcloud.cn>
2020-05-15 17:53:26 +0800

    两数之和的再讨论

--- src/main/java/ds/TwoSum.java
+++ src/main/java/ds/TwoSum.java
@@ -1,5 +1,6 @@
 package ds;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,36 +24,82 @@ import java.util.Map;
 public class TwoSum {
 
     /**
+     * 解法1:
+     * 散列表解法
+     * <p>
      * k为具体的值
      * v为值对应的index
      * <p>
      * 关键数据结构就是散列表，用来存储target与目标元素的差，做配对用。
+     * 时间复杂度为O(n),空间复杂度为O(n),因为new了额外的散列表，显然不是最优解
      */
-    private Map<Integer, Integer> map = new HashMap<>();
-
-    public int[] twoSum(int[] nums, int target) {
-        int index[] = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
+    private int[] twoSumByMap(int[] array, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] index = new int[2];
+        for (int i = 0; i < array.length; i++) {
+            if (map.containsKey(array[i])) {
                 //返回之前配对的index
-                index[0] = map.get(nums[i]);
+                index[0] = map.get(array[i]);
                 //返回当前的index
                 index[1] = i;
             } else {
-                map.put(target - nums[i], i);
+                map.put(target - array[i], i);
             }
         }
         return index;
+    }
+
+    /**
+     * 解法2:
+     * 暴力穷举解法
+     * 时间复杂度为O(n^2),空间复杂度为O(1),平方阶恐怖的时间复杂度，显然也不是最优解
+     */
+    private int[] twoSumByEnum(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] + array[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    /**
+     * 解法3:
+     * 二分查找变种
+     * 时间复杂度O(n),空间复杂度O(1),采用双指针的思维实现,为最优解。
+     * 但是因为采用的是二分查找,所以有个前提,数据源的数组,必须有序
+     */
+    private int[] twoSumByTwoPointer(int[] array, int target) {
+        int start = 0;
+        int end = array.length - 1;
+        while (start < end) {
+            if (array[start] + array[end] > target) {
+                end--;
+            } else if (array[start] + array[end] < target) {
+                start++;
+            } else {
+                return new int[]{start, end};
+            }
+        }
+        return new int[]{-1, -1};
     }
 
     public static void main(String[] args) {
         int[] ints = {2, 7, 11, 15};
         TwoSum twoSum = new TwoSum();
         int target = 9;
-        int[] indexs = twoSum.twoSum(ints, target);
-        System.out.println("两数之和为" + target + "的索引为:");
-        for (int index : indexs) {
-            System.out.println(index);
-        }
+        int[] indexs = twoSum.twoSumByMap(ints, target);
+        System.out.println("两数之和为" + target + "的索引为:" + Arrays.toString(indexs));
+        System.out.println();
+        int[] indexs2 = twoSum.twoSumByEnum(ints, target);
+        System.out.println("两数之和为" + target + "的索引为:" + Arrays.toString(indexs2));
+        System.out.println();
+        //采用二分查找的方法时，保证输入的数据有序，所以得先排一下序
+        InsertSort insertSort = new InsertSort();
+        insertSort.insertSort(ints);
+        int[] indexs3 = twoSum.twoSumByTwoPointer(ints, target);
+        System.out.println("两数之和为" + target + "的索引为:" + Arrays.toString(indexs3));
     }
 }
