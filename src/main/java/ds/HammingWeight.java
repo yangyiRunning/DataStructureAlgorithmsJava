1d004fc8a9764db749e2bc291fb0fb516e6c7057
yangyi <yangyirunning@163.com>
2020-12-08 00:38:42 +0800

    位1的个数

--- src/main/java/ds/HammingWeight.java
+++ src/main/java/ds/HammingWeight.java
@@ -2,32 +2,7 @@ package ds;
 
 /**
  * 位1的个数
- * <p>
- * 编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
- * <p>
- * <p>
- * <p>
- * 示例 1：
- * <p>
- * 输入：00000000000000000000000000001011
- * 输出：3
- * 解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
- * 示例 2：
- * <p>
- * 输入：00000000000000000000000010000000
- * 输出：1
- * 解释：输入的二进制串 00000000000000000000000010000000 中，共有一位为 '1'。
- * 示例 3：
- * <p>
- * 输入：11111111111111111111111111111101
- * 输出：31
- * 解释：输入的二进制串 11111111111111111111111111111101 中，共有 31 位为 '1'。
- * <p>
- * <p>
- * 提示：
- * <p>
- * 请注意，在某些语言（如 Java）中，没有无符号整数类型。在这种情况下，输入和输出都将被指定为有符号整数类型，并且不应影响您的实现，因为无论整数是有符号的还是无符号的，其内部的二进制表示形式都是相同的。
- * 在 Java 中，编译器使用二进制补码记法来表示有符号整数。因此，在上面的 示例 3 中，输入表示有符号整数 -3。
+ * LeetCode 191 https://leetcode-cn.com/problems/number-of-1-bits/
  *
  * @author yangyi 2019年02月01日17:47:47
  */
@@ -48,8 +23,8 @@ public class HammingWeight {
         int b = 0b00000000000000000000000010000000;
         int c = 0b11111111111111111111111111111101;
         HammingWeight hammingWeight = new HammingWeight();
-        System.out.println(String.valueOf(a) + "中1的个数为:" + hammingWeight.hammingWeight(a));
-        System.out.println(String.valueOf(b) + "中1的个数为:" + hammingWeight.hammingWeight(b));
-        System.out.println(String.valueOf(c) + "中1的个数为:" + hammingWeight.hammingWeight(c));
+        System.out.println(a + "中1的个数为:" + hammingWeight.hammingWeight(a));
+        System.out.println(b + "中1的个数为:" + hammingWeight.hammingWeight(b));
+        System.out.println(c + "中1的个数为:" + hammingWeight.hammingWeight(c));
     }
 }
