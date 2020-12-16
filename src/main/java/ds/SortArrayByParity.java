3c407717f86f44181d2e6ccefb1c204f095fa8ce
yangyi <yangyirunning@163.com>
2020-12-16 09:29:11 +0800

    按奇偶排序数组

--- src/main/java/ds/SortArrayByParity.java
+++ src/main/java/ds/SortArrayByParity.java
@@ -3,44 +3,36 @@ package ds;
 import java.util.Arrays;
 
 /**
- * 给定一个非负整数数组 A，返回一个由 A 的所有偶数元素组成的数组，后面跟 A 的所有奇数元素。
- * <p>
- * 你可以返回满足此条件的任何数组作为答案。
- * <p>
- * <p>
- * <p>
- * 示例：
- * <p>
- * 输入：[3,1,2,4]
- * 输出：[2,4,3,1]
- * 输出 [4,2,3,1]，[2,4,1,3] 和 [4,2,1,3] 也会被接受。
- * <p>
- * <p>
- * 提示：
- * <p>
- * 1 <= A.length <= 5000
- * 0 <= A[i] <= 5000
+ * 按奇偶排序数组
+ * LeetCode https://leetcode-cn.com/problems/sort-array-by-parity/
+ *
+ * @author yangyi 2020年12月15日14:12:15
  */
 public class SortArrayByParity {
 
     public int[] sortArrayByParity(int[] A) {
         if (A == null) {
-            return new int[0];
+            return new int[]{};
         }
         if (A.length == 0) {
             return A;
         }
         int start = 0;
         int end = A.length - 1;
-        int[] result = new int[A.length];
-        for (int i = 0; i < A.length; i++) {
-            if (A[i] % 2 == 0) {
-                result[start++] = A[i];
-            } else {
-                result[end--] = A[i];
+        while (start < end) {
+            if (A[start] % 2 == 0) {
+                start++;
+                continue;
             }
+            if (A[end] % 2 != 0) {
+                end--;
+                continue;
+            }
+            int temp = A[start];
+            A[start] = A[end];
+            A[end] = temp;
         }
-        return result;
+        return A;
     }
 
     public static void main(String[] args) {
