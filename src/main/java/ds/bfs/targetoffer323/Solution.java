79c190e89cf3988d6ced8b9aff15aa91421ff1e0
yangyi <yangyirunning@163.com>
2021-05-02 00:03:40 +0800

    剑指 Offer 32 - III  从上到下打印二叉树 III

--- src/main/java/ds/bfs/targetoffer323/Solution.java
+++ src/main/java/ds/bfs/targetoffer323/Solution.java
@@ -1,4 +1,4 @@
-package ds.bfs.targetoffer322;
+package ds.bfs.targetoffer323;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -6,29 +6,29 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * 从上到下打印二叉树 II
- * 剑指 Offer 32 - II. https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
+ * 从上到下打印二叉树 III
+ * 剑指 Offer 32 - III. https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/
  *
- * @author yangyi 2021年04月30日18:19:43
+ * @author yangyi 2021年04月30日18:19:54
  */
 public class Solution {
 
     public class TreeNode {
         int val;
-        TreeNode left;
-        TreeNode right;
+        Solution.TreeNode left;
+        Solution.TreeNode right;
 
         TreeNode(int x) {
             val = x;
         }
     }
 
-    private TreeNode createTree() {
-        TreeNode node_3 = new TreeNode(3);
-        TreeNode node_9 = new TreeNode(9);
-        TreeNode node_20 = new TreeNode(20);
-        TreeNode node_15 = new TreeNode(15);
-        TreeNode node_7 = new TreeNode(7);
+    private Solution.TreeNode createTree() {
+        Solution.TreeNode node_3 = new Solution.TreeNode(3);
+        Solution.TreeNode node_9 = new Solution.TreeNode(9);
+        Solution.TreeNode node_20 = new Solution.TreeNode(20);
+        Solution.TreeNode node_15 = new Solution.TreeNode(15);
+        Solution.TreeNode node_7 = new Solution.TreeNode(7);
         node_3.left = node_9;
         node_3.right = node_20;
         node_20.left = node_15;
@@ -36,20 +36,25 @@ public class Solution {
         return node_3;
     }
 
-    public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> levelOrder(Solution.TreeNode root) {
         if (root == null) {
             return new LinkedList<>();
         }
         List<List<Integer>> result = new LinkedList<>();
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
+        int level = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
-            List<Integer> levelList = new LinkedList<>();
+            LinkedList<Integer> levelList = new LinkedList<>();
             for (int i = 0; i < size; i++) {
                 TreeNode cur = queue.poll();
                 if (cur != null) {
-                    levelList.add(cur.val);
+                    if (level % 2 == 0) {
+                        levelList.addLast(cur.val);
+                    } else {
+                        levelList.addFirst(cur.val);
+                    }
                 }
                 if (cur.left != null) {
                     queue.offer(cur.left);
@@ -59,6 +64,7 @@ public class Solution {
                 }
             }
             result.add(levelList);
+            level++;
         }
         return result;
     }
