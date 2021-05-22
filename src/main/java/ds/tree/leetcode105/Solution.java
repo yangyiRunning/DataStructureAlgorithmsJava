51bb068a2feb42febdf3b4b15ae8f18482b0af0b
yangyi <yangyirunning@163.com>
2021-05-22 23:52:12 +0800

    剑指Offer 07.重建二叉树

--- src/main/java/ds/tree/leetcode105/Solution.java
+++ src/main/java/ds/tree/leetcode105/Solution.java
@@ -3,6 +3,7 @@ package ds.tree.leetcode105;
 /**
  * 从前序与中序遍历序列构造二叉树
  * Leetcode 105 https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+ * 剑指 Offer 07 https://leetcode-cn.com/problems/zhong-jian-er-cha-shu-lcof/
  *
  * @author yangyi 2020年11月29日14:41:50
  */
@@ -22,7 +23,7 @@ public class Solution {
      * 前序遍历 preorder = [3,9,20,15,7]
      * 中序遍历 inorder = [9,3,15,20,7]
      */
-    private TreeNode buildTree(int[] preorder, int[] inorder) {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
         return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1);
     }
 
