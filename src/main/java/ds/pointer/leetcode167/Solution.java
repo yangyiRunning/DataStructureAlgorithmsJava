7836858bb2186f1eba447bcd608b43a71af50a70
yangyi <yangyi@rongcloud.cn>
2022-04-24 18:55:32 +0800

    167. 两数之和 II - 输入有序数组

--- src/main/java/ds/pointer/leetcode167/Solution.java
+++ src/main/java/ds/pointer/leetcode167/Solution.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
  * 两数之和 II - 输入有序数组
  * LeetCode 167 https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
  *
- * @author yangyi 2020年12月14日11:52:36
+ * @author yangyi 2022年04月24日18:47:00
  */
 public class Solution {
 
@@ -15,12 +15,12 @@ public class Solution {
         int end = numbers.length - 1;
         while (start < end) {
             int sum = numbers[start] + numbers[end];
-            if (sum == target) {
-                return new int[]{start + 1, end + 1};
-            } else if (sum < target) {
+            if (sum < target) {
                 start++;
             } else if (sum > target) {
                 end--;
+            } else if (sum == target) {
+                return new int[]{start + 1, end + 1};
             }
         }
         return new int[]{-1, -1};
@@ -28,7 +28,7 @@ public class Solution {
 
     public static void main(String[] args) {
         int[] a = {2, 7, 11, 15};
-        Solution twoSumII = new Solution();
-        System.out.println(Arrays.toString(a) + "数组中和为9对应的数的索引为:" + Arrays.toString(twoSumII.twoSum(a, 9)));
+        Solution twoSum2 = new Solution();
+        System.out.println(Arrays.toString(a) + "数组中和为9对应的数的索引为:" + Arrays.toString(twoSum2.twoSum(a, 9)));
     }
 }
