e88d651e31be499e69a1a45d7fa9a735e5479475
yangyi <yangyirunning@163.com>
2022-05-12 22:14:35 +0800

    287. 寻找重复数

--- src/main/java/ds/pointer/leetcode287/Solution.java
+++ src/main/java/ds/pointer/leetcode287/Solution.java
@@ -9,8 +9,7 @@ package ds.pointer.leetcode287;
 public class Solution {
 
     public int findDuplicate(int[] nums) {
-        int slow = 0;
-        int fast = 0;
+        int slow = 0, fast = 0;
         slow = nums[slow];
         fast = nums[nums[fast]];
         while (slow != fast) {
